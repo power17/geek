bc0c1f334c00d76a4ede5d8bc34568cd5eb72909
power <power@powerdeMacBook-Pro.local>
2023-04-09 17:55:44 +0800

    单元测试

---
+++ packages/utils/babel.js
@@ -0,0 +1,3 @@
+const a = '123' 
+const add = (a,b) => a + b 
+const set = new Set([1,2,3])
---
+++ packages/utils/src/__test__/add.spec.js
@@ -0,0 +1,9 @@
+const add = require('../add.js')
+describe('测试Add函数', () => {
+	test("add(1,2) === 3", ()=> {
+		expect(add(1,2)).toBe(3)
+	} )
+	test("add(2,2) === 4", ()=> {
+		expect(add(2,2)).toBe(4)
+	} )
+})
