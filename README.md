d57ec448df61172d7a6ae1b688bb65d1ce5ea806
power <power@powerdeMacBook-Pro.local>
2023-04-13 21:15:46 +0800

    rollup understand

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #  极客笔记
 ## 思维导图制作网站  
 # [](https://www.processon.com/view/link/61d42479e0b34d1be7d89dba#map)
+# AST观察
+# [](https://astexplorer.net)
--- packages/babel/index.js
+++ packages/babel/index.js
@@ -1,5 +1,8 @@
+// 生成AST语法树
 const parser = require('@babel/parser')
+// 爬取AST语法树
 const traverse = require('@babel/traverse').default
+// 将AST还原成代码
 const generator = require('@babel/generator').default
 const types = require('@babel/types')
 const fs = require('fs')
@@ -7,3 +10,12 @@ const source = fs.readFileSync('./source.ts').toString()
 
 const ast = parser.parse(source, { plugins: ['typescript', 'jsx'] })
 console.log(ast)
+traverse(ast, {
+	CallExpression(path) {
+		const  calleeStr = generator(path.node.callee).code 
+		console.log(calleeStr)
+		if (['console.log', 'console.error'].includes(calleeStr)) { 
+			const { line, column } = path.node.loc.start
+			 path.node.arguments.unshift(types.stringLiteral(`${fileName}(${ }
+	}
+})
